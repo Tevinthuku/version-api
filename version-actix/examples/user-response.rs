d95c277c3d026ff6ffd21ae9371c5d28e13818a0
Tevinthuku <tevinthuku@gmail.com>
2026-03-06 20:57:45 +0300

    restructure files

--- version-actix/examples/user-response.rs
+++ version-actix/examples/user-response.rs
@@ -1,21 +1,20 @@
-use crate::responder::VersionedJsonResponder;
 use actix_web::{Result, get, web};
 use serde::Deserialize;
 use serde::Serialize;
+use version_actix::VersionedJsonResponder;
 use version_core::{
     ApiVersionId, ChangeHistory, VersionChange, registry::ApiResponseResourceRegistry,
 };
-mod responder;
 
 #[derive(Serialize, Deserialize)]
-struct MyObj {
+struct CurrentUser {
     first_name: String,
     last_name: String,
 }
 
 #[get("/a/{name}")]
-async fn index(name: web::Path<String>) -> Result<VersionedJsonResponder<MyObj>> {
-    let obj = MyObj {
+async fn index(name: web::Path<String>) -> Result<VersionedJsonResponder<CurrentUser>> {
+    let obj = CurrentUser {
         first_name: name.to_string(),
         last_name: name.to_string(),
     };
@@ -27,7 +26,7 @@ async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
 
     let mut registry = ApiResponseResourceRegistry::new("X-API-Version".to_string());
-    MyObjResponseHistoryVersions::register(&mut registry).unwrap();
+    CurrentUserResponseHistoryVersions::register(&mut registry).unwrap();
     let registry = web::Data::new(registry);
     HttpServer::new(move || App::new().service(index).app_data(registry.clone()))
         .bind(("127.0.0.1", 8080))?
@@ -36,7 +35,7 @@ async fn main() -> std::io::Result<()> {
 }
 
 #[derive(ApiVersionId)]
-pub enum MyApiVersions {
+pub enum ApiVersion {
     #[version("2.0.0")]
     V2_0_0,
     #[version("1.0.0")]
@@ -44,22 +43,22 @@ pub enum MyApiVersions {
     #[version("0.9.0")]
     V0_9_0,
 }
-// build version change history for MyObj
+
 #[derive(ChangeHistory)]
-#[head(MyObj)]
+#[head(CurrentUser)]
 #[changes(
-    below(MyApiVersions::V2_0_0) => CollapseNamesToSingleOne,
+    below(ApiVersion::V2_0_0) => UserWithSingleNameField,
 )]
-struct MyObjResponseHistoryVersions;
+struct CurrentUserResponseHistoryVersions;
 
 #[derive(VersionChange, Serialize, Deserialize)]
-#[description = "Names will now render a single string instead of two separate strings"]
-struct CollapseNamesToSingleOne {
+#[description = "Users on less than v2.0.0 expect a single string for the name"]
+struct UserWithSingleNameField {
     name: String,
 }
 
-impl From<MyObj> for CollapseNamesToSingleOne {
-    fn from(obj: MyObj) -> Self {
+impl From<CurrentUser> for UserWithSingleNameField {
+    fn from(obj: CurrentUser) -> Self {
         Self {
             name: format!("{} {}", obj.first_name, obj.last_name),
         }

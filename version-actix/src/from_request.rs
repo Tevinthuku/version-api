dd3d6631b9b84368d2e547d76c213abfb5d559dc
Tevinthuku <tevinthuku@gmail.com>
2026-03-19 21:27:53 +0300

    init VersionedJsonRequest struct

---
+++ version-actix/src/from_request.rs
@@ -0,0 +1,47 @@
+use std::pin::Pin;
+
+use actix_web::{
+    Error,
+    web::{self, Json},
+};
+use actix_web::{FromRequest, HttpRequest, dev::Payload};
+use serde::{Serialize, de::DeserializeOwned};
+use version_core::registry::{ResourceRegistry, TransformDirection};
+
+use crate::ActixVersionIdExtractor;
+
+pub struct VersionedJsonRequest<T: DeserializeOwned + Serialize + 'static>(pub T);
+
+impl<T: DeserializeOwned + Serialize + 'static> FromRequest for VersionedJsonRequest<T> {
+    type Error = Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    fn from_request(req: &HttpRequest, payload: &mut Payload) -> Self::Future {
+        let req = req.clone();
+        let mut payload = payload.take();
+        Box::pin(async move {
+            let registry = req.app_data::<web::Data<ResourceRegistry>>();
+            let version_id_extractor = req.app_data::<web::Data<dyn ActixVersionIdExtractor>>();
+            let json_body = Json::<serde_json::Value>::from_request(&req, &mut payload).await?;
+
+            if let Some((registry, version_extractor)) = registry.zip(version_id_extractor) {
+                let version_id = version_extractor.extract(&req)?;
+                if let Some(version_id) = version_id {
+                    let transformed_body = registry.transform(
+                        json_body.0,
+                        TransformDirection::UpForRequests {
+                            user_version: version_id,
+                        },
+                    )?;
+
+                    let transformed_body = serde_json::from_slice::<T>(&transformed_body)?;
+
+                    return Ok(VersionedJsonRequest(transformed_body));
+                }
+            }
+
+            let json_body = serde_json::from_value::<T>(json_body.0)?;
+            Ok(VersionedJsonRequest(json_body))
+        })
+    }
+}
--- version-actix/src/lib.rs
+++ version-actix/src/lib.rs
@@ -1,5 +1,6 @@
 mod extractors;
 mod responder;
+mod from_request;
 pub use extractors::ActixVersionIdExtractor;
 pub use extractors::BaseActixVersionIdExtractor;
 pub use responder::VersionedJsonResponder;
